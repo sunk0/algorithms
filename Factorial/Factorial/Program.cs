99572570ee57a252c3e370f552e3e311e8144e98
Alexander Savov <sunk0@abv.bg>
2019-12-04 19:07:07 +0200

    factorial

--- Factorial/Factorial/Program.cs
+++ Factorial/Factorial/Program.cs
@@ -6,19 +6,20 @@ namespace Factorial
     {
         static void Main(string[] args)
         {
-
             int input = int.Parse(Console.ReadLine());
-            
             Console.WriteLine(Factorial(input));
         }
         static int Factorial(int n)
         {
-            if(n==1){
+            int output;
+            if (n==1){
                 return 1;
             }
-            int output;
-            output = n * Factorial(n-1);
-            return output;
+            else
+            {
+                output = n * Factorial(n - 1);
+                return output;
+            }          
         }
     }
 }
